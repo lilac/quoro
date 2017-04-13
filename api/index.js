f9c280cf228970450d6f0493c8e28952cc68363e
Junjun Deng <cnjamesdeng@gmail.com>
2017-04-13 16:21:33 +0800

    Etc: move api specific files to a folder

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import {Server} from "http";
+import Express from "express";
+import apiRoutes from "./features/index";
+
+const app = Express();
+const apiServer = Server(app);
+
+// global.socket = io(apiServer);
+
+app.use('/api', apiRoutes);
+
+const port = parseInt(process.env.API_SERVER_PORT, 10) || 8000;
+
+apiServer.listen(port, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    return console.info(`Server is running at localhost:${port}`);
+});
--- src/server.js
+++ src/server.js
@@ -3,19 +3,17 @@ import path from 'path';
 import Express from 'express';
 import bodyParser from 'body-parser';
 import io from 'socket.io';
-import apiRoutes from './features/index';
 import serverRendering from './server-rendering';
 
 const app = Express();
 const appServer = Server(app);
 
-global.socket = io(appServer);
+// global.socket = io(appServer);
 
 app.use('/static', Express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
 
-app.use('/api', apiRoutes);
 app.get('*', serverRendering);
 
 export default appServer;
